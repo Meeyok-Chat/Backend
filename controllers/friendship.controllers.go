4ea31d4a20e7e69c5c20811f0a6bf60cb4e930b8
NasmeenI <nasmeen2503@gmail.com>
2025-03-26 02:49:20 +0700

    feat: swagger for friendship

--- controllers/friendship.controllers.go
+++ controllers/friendship.controllers.go
@@ -24,6 +24,19 @@ func NewFriendshipController(friendshipService service.FriendshipService) Friend
 	}
 }
 
+// AddFriendshipHandler godoc
+// @Summary      Send a friend request
+// @Description  Sends a friend request from one user to another
+// @Tags         friendship
+// @Accept       json
+// @Produce      json
+// @Param        id1   query     string  true  "User ID of the requester"
+// @Param        id2   query     string  true  "User ID of the recipient"
+// @Security     Bearer
+// @Success      200   {object}  models.Friendship
+// @Failure      400   {object}  models.HTTPError
+// @Failure      500   {object}  models.HTTPError
+// @Router       /friendships [post]
 func (c *friendshipController) AddFriendshipHandler(ctx *gin.Context) {
 	userID1 := ctx.Query("id1")
 	userID2 := ctx.Query("id2")
@@ -37,6 +50,18 @@ func (c *friendshipController) AddFriendshipHandler(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, friendship)
 }
 
+// AcceptFriendshipHandler godoc
+// @Summary      Accept a friend request
+// @Description  Accepts a pending friend request by updating the status
+// @Tags         friendship
+// @Accept       json
+// @Produce      json
+// @Param        id   path      string  true  "Friendship ID"
+// @Security     Bearer
+// @Success      200   {object}  models.Friendship
+// @Failure      400   {object}  models.HTTPError
+// @Failure      500   {object}  models.HTTPError
+// @Router       /friendships/{id}/accept [patch]
 func (c *friendshipController) AcceptFriendshipHandler(ctx *gin.Context) {
 	friendshipID := ctx.Param("id")
 
@@ -49,6 +74,18 @@ func (c *friendshipController) AcceptFriendshipHandler(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, friendship)
 }
 
+// RejectFriendshipHandler godoc
+// @Summary      Reject a friend request
+// @Description  Rejects a pending friend request and removes it from the system
+// @Tags         friendship
+// @Accept       json
+// @Produce      json
+// @Param        id   path      string  true  "Friendship ID"
+// @Security     Bearer
+// @Success      200   {object}  models.Friendship
+// @Failure      400   {object}  models.HTTPError
+// @Failure      500   {object}  models.HTTPError
+// @Router       /friendships/{id}/reject [patch]
 func (c *friendshipController) RejectFriendshipHandler(ctx *gin.Context) {
 	friendshipID := ctx.Param("id")
 
