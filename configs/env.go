c0622d92eb98be5f251cc12d6f566c6b76e1ee95
NasmeenI <nasmeen2503@gmail.com>
2025-04-14 21:57:21 +0700

    fix: env config

--- configs/env.go
+++ configs/env.go
@@ -1,16 +1,13 @@
 package configs
 
 import (
-	"log"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 func GetEnv(envVariable string) string {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
 	return os.Getenv(envVariable)
 }
